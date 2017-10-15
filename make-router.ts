627f97a60630df7c12dbf5a9b99c63f2b7935925
M3ssman <uwe.hartwig@bitsrc.info>
2017-10-15 12:26:50 +0200

    enable post requests

--- make-router.ts
+++ make-router.ts
@@ -11,54 +11,46 @@ export class MakeRouter {
 
     router: express.Router;
 
-    get(req: express.Request, res: express.Response, next: express.NextFunction): void {
-        console.log('get query: ' + JSON.stringify(req.query));
+    /**
+     * 
+     * Handle GET Request
+     * 
+     * @param req 
+     * @param res 
+     */
+    get(req: express.Request, res: express.Response): void {
+        console.log('GET with '+ req);
+        // prepare metadata from query
+        const label = req.query.label || 'Mathematik :: Sienna Metzner, 3c';
+        const metaData = prepareMetaData(label);
+
+        // inspespect exercises query parameter
         const parameters = req.query;
         const types = determineExerciseTypes(parameters.types);
-        const label = parameters.label || 'Mathematik :: Sienna Metzner, 3c';
         const exerciseTypes = types || [defaultAdd];
 
+        // process exercises
+        processExercisesPromise(exerciseTypes, metaData, res);
+    }
 
-        let doc = new PDFDocument({ 'size': [600, 840] });
-        doc.font('Courier-Bold', 'Courier', 14);
-        moment.locale('de');
-        const datum = moment(new Date()).format('LL');
+    /**
+     * 
+     * Handle POST Request
+     * 
+     * @param req 
+     * @param res 
+     */
+    post(req: express.Request, res: express.Response): void {
+        console.log('POST with '+ req);
+        let exercises = req.body.exercises;
+        const label = req.body.label || 'Mathematik :: Sienna Metzner, 3c';
+        const metaData = prepareMetaData(label);
+        if(exercises === undefined) {
+            exercises = [defaultAdd];
+        }
 
-        const metaData = {
-            datum: datum, label: label
-        };
-        console.log('metadata: ' + JSON.stringify(metaData));
-
-        let a = 1;
-        doc.text(label);
-        doc.text('(1) ' + datum);
-        let y = 150;
-        let x = 40;
-        const ePromise = makeSet(exerciseTypes);
-        ePromise.then(
-            op => {
-                console.log('resolved exercises: ' + JSON.stringify(op));
-                const exc: ExerciseMathImpl[][] = op;
-                for (let i = 0; i < exc.length; i++) {
-                    let row = a + ' ) ';
-                    doc.text(row, x, y);
-                    x += 40;
-                    for (let j = 0; j < exc[i].length; j++) {
-                        const excR: string = exc[i][j].get()[0];
-                        doc.text(excR, x, y);
-                        y += 40;
-                    }
-                    a++;
-                    x += 120;
-                    y = 150;
-                }
-                doc.pipe(res);
-                doc.end();
-            },
-            err => {
-                console.log('rejected promise : ' + err);
-            }
-        );
+        // process exercises
+        processExercisesPromise(exercises, metaData, res);
     }
 
     /**
@@ -71,7 +63,14 @@ export class MakeRouter {
 
     init() {
         this.router.get('/', this.get);
+        this.router.post('/',this.post)
     }
+}
+
+function prepareMetaData(label: string): MetaData {
+    moment.locale('de');
+    const datum = moment(new Date()).format('LL');
+    return { datum: datum, label: label };
 }
 
 function determineExerciseTypes(types: string): any[] | undefined {
@@ -86,6 +85,55 @@ function determineExerciseTypes(types: string): any[] | undefined {
         return t;
     }
 }
+
+function processExercisesPromise(exerciseTypes: any[], metaData: MetaData, res: express.Response) {
+    // request exercises
+    makeSet(exerciseTypes).then(op => {
+        // prepare pdf doc
+        let doc = new PDFDocument({ 'size': [600, 840] });
+        doc.font('Courier-Bold', 'Courier', 14);
+        if (metaData) {
+            if (metaData.label) {
+                doc.text(metaData.label);
+            }
+            if (metaData.datum) {
+                doc.text('(1) ' + metaData.datum);
+            }
+        }
+        // prepare width, indents, nr of exercises
+        let y = 150;
+        let x = 40;
+        let a = 1;
+        console.log('resolved exercises: ' + JSON.stringify(op));
+        const exc: ExerciseMathImpl[][] = op;
+        for (let i = 0; i < exc.length; i++) {
+            let row = a + ' ) ';
+            doc.text(row, x, y);
+            x += 40;
+            for (let j = 0; j < exc[i].length; j++) {
+                const excR: string = exc[i][j].get()[0];
+                doc.text(excR, x, y);
+                y += 40;
+            }
+            a++;
+            x += 120;
+            y = 150;
+        }
+        doc.pipe(res);
+        doc.end();
+    }, err => {
+        console.log('promise rejected: ' + err);
+    });
+}
+
+/**
+ * Util Type
+ */
+interface MetaData {
+    label: string;
+    datum: string;
+}
+
 
 const makeRoute = new MakeRouter();
 export default makeRoute.router;
