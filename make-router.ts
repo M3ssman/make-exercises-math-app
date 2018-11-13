5d395f04dd1284cbdb3b49fe96d1a4dec61b20be
M3ssman <uwe.hartwig@bitsrc.info>
2018-11-13 21:53:08 +0100

    pdf rendering for fractions

--- make-router.ts
+++ make-router.ts
@@ -2,20 +2,20 @@ import * as express from 'express';
 import * as PDFDocument from 'pdfkit';
 import * as moment from 'moment';
 
-import * as extype 
-    from 'make-exercises-math';
-//from '../make-exercises-math';
-import { 
-    makeSet, 
+import * as extype
+    // from 'make-exercises-math';
+    from '../make-exercises-math';
+import {
+    makeSet,
     Exercise,
     ExerciseSet,
     Options,
     Rendered
-} from 'make-exercises-math';
-//} from '../make-exercises-math';
+    // } from 'make-exercises-math';
+} from '../make-exercises-math';
 
 // some fallback
-const defaultAdd : Options = {
+const defaultAdd: Options = {
     set: "N",
     operations: ["add"]
 };
@@ -36,7 +36,7 @@ export class MakeRouter {
         const metaData = prepareMetaData(label);
         const parameters = req.query;
         const configs = getExerciseTypes(parameters.types);
-        processExercisesPromise( configs, metaData, res);
+        processExercisesPromise(configs, metaData, res);
     }
 
     /**
@@ -63,7 +63,7 @@ export class MakeRouter {
 
     init() {
         this.router.get('/', this.get);
-        this.router.post('/',this.post)
+        this.router.post('/', this.post)
     }
 }
 
@@ -74,11 +74,11 @@ function prepareMetaData(label: string): MetaData {
 }
 
 function getExerciseTypes(types: string): Options[] {
-    if (types ) {
+    if (types) {
         const typesArray = types.split(',');
         return typesArray
-            .filter( type => extype[type] !== undefined)
-            .map( t =>  extype[t]);
+            .filter(type => extype[type] !== undefined)
+            .map(t => extype[t]);
     } else {
         return [defaultAdd];
     }
@@ -93,10 +93,11 @@ function getExerciseTypes(types: string): Options[] {
  * @param res 
  */
 function processExercisesPromise(exerciseTypes: any[], metaData: MetaData, res: express.Response) {
+    console.debug('[DEBUG] - request exercises from ' + JSON.stringify(exerciseTypes) + ' with metadata: ' + JSON.stringify(metaData))
     // request exercises
     makeSet(exerciseTypes)
-        .then(promisedSets => renderExercisesPDF(promisedSets,metaData,res), 
-              err => console.log('exercises not resolved: ' + err))
+        .then(promisedSets => renderExercisesPDF(promisedSets, metaData, res),
+            err => console.log('exercises not resolved: ' + err))
         .catch(err => {
             if (console) {
                 console.log(err);
@@ -104,7 +105,7 @@ function processExercisesPromise(exerciseTypes: any[], metaData: MetaData, res: 
         });
 }
 
-function renderExercisesPDF(sets: ExerciseSet[],metaData: MetaData, res: express.Response) {
+function renderExercisesPDF(sets: ExerciseSet[], metaData: MetaData, res: express.Response) {
     // prepare pdf doc
     let doc = new PDFDocument({ 'size': [600, 840] });
     doc.font('Courier-Bold', 'Courier', 16);
@@ -125,14 +126,14 @@ function renderExercisesPDF(sets: ExerciseSet[],metaData: MetaData, res: express
         let row = a + ' ) ';
         doc.text(row, x, y);
         x += 40;
-        set.exercises.forEach((exc: Exercise) => {    
+        set.exercises.forEach((exc: Exercise) => {
             const rendered: Rendered[] = exc.rendered;
             if (typeof rendered === 'object') {
-                if(rendered.length === 1) {
+                if (rendered.length === 1) {
                     doc.text(rendered[0].rendered, x, y);
                     y += 40;
-                // having at least an additional carry row ...
-                } else if(rendered.length > 1) {
+                    // having at least an additional carry row ...
+                } else if (rendered.length > 1) {
                     y = handleRendered(rendered, x, doc, y);
                     // guess some space between each exercise
                     y += 15;
@@ -141,7 +142,7 @@ function renderExercisesPDF(sets: ExerciseSet[],metaData: MetaData, res: express
         });
         a++;
         // if the last entry from last row was division ...
-        if(set.properties.extension === 'DIV_EVEN') {
+        if (set.properties.extension === 'DIV_EVEN') {
             x += 200;
         } else {
             x += 120;
@@ -162,19 +163,21 @@ interface MetaData {
 
 function handleRendered(rendered: extype.Rendered[], x: number, doc: PDFKit.PDFDocument, y: number) {
     for (let k = 0; k < rendered.length; k++) {
-        let row:Rendered = rendered[k];
-        if(! row) {
-            console.log('[ERROR] entry '+k+': encountered row of type "undefined" from rendered '+JSON.stringify(rendered)+'!')
+        let row: Rendered = rendered[k];
+        let _type: extype.RenderedType = row.type
+        if (!row) {
+            console.log('[ERROR] entry ' + k + ': encountered row of type "undefined" from rendered ' + JSON.stringify(rendered) + '!')
             continue;
         }
-        // check row for marks of former digits <> 0
+        // check row for masked marks of former digits
         if (row.rendered.indexOf('?') > -1) {
             let _x = x;
             for (let l = 0; l < row.rendered.length; l++) {
                 // replace underscore mark by rectangle
                 if (row.rendered[l] === '?') {
-                    doc.lineWidth(1);
-                    doc.rect(_x, y, 8, 12).stroke();
+                    doc.lineWidth(1)
+                    doc.strokeColor('black')
+                    doc.rect(_x, y, 8, 12).stroke()
                 }
                 else {
                     doc.text(row.rendered[l], _x, y);
@@ -182,25 +185,58 @@ function handleRendered(rendered: extype.Rendered[], x: number, doc: PDFKit.PDFD
                 _x += 10;
             }
             _x = 0;
-        }
-        else {
+        } else if (_type !== 'FRACTION_STRIKE') {
             // no marks, just render line as it is
             doc.text(row.rendered, x, y);
         }
         // strike line before result entry
-        if (k === rendered.length - 2) {
-            const w = rendered[k].rendered.length * 10;
-            doc.lineWidth(2);
-            doc.moveTo(x, y + 14).lineTo(x + w, y + 14).stroke();
-            y += 5;
-        }
-        // srike twice below every result row
-        if (k === rendered.length - 1) {
-            const w = rendered[k].rendered.length * 10;
-            doc.lineWidth(1);
-            doc.moveTo(x, y + 14).lineTo(x + w, y + 14).stroke();
-            doc.moveTo(x, y + 16).lineTo(x + w, y + 16).stroke();
-            y += 5;
+        if (_type.startsWith('FRACTION')) {
+            if (_type === 'FRACTION_STRIKE') {
+                const verticalSpace = 5
+                let _x = x
+                let x1 = x
+                let x2 = x
+
+                let metSign = false
+                for (let l = 0; l < row.rendered.length; l++) {
+                    const _char = row.rendered[l]
+                    x2 += 10
+
+                    if (_isSign(_char)) {
+                        doc.text(_char, _x, y);
+                        metSign = true
+                    } else {
+                        // lately came accross sign?
+                        if (metSign) {
+                            x2 = _x - 10
+                            doc.lineWidth(2)
+                            doc.strokeColor('green')
+                            doc.moveTo(x1 + 1, y + verticalSpace).lineTo(x2 - 1, y + verticalSpace).stroke()
+                            x1 = _x
+                            metSign = false
+                        }
+                    }
+                    _x += 10
+                }
+                // final stroke
+                doc.moveTo(x1, y + verticalSpace).lineTo(_x, y + verticalSpace).stroke()
+            }
+        } else {
+            // no Fractions, but add_carry, sub, mult or div_even
+            if (k === rendered.length - 2) {
+                const w = rendered[k].rendered.length * 10;
+                doc.lineWidth(2);
+                doc.moveTo(x, y + 14).lineTo(x + w, y + 14).stroke();
+                y += 5;
+            }
+            // srike twice below every result row
+            if (k === rendered.length - 1) {
+                const w = rendered[k].rendered.length * 10;
+                doc.lineWidth(1);
+                doc.moveTo(x, y + 14).lineTo(x + w, y + 14).stroke();
+                doc.moveTo(x, y + 16).lineTo(x + w, y + 16).stroke();
+                y += 5;
+            }
         }
         // next row
         y += 15;
@@ -208,3 +244,6 @@ function handleRendered(rendered: extype.Rendered[], x: number, doc: PDFKit.PDFD
     return y;
 }
 
+function _isSign(s: string): boolean {
+    return s === '+' || s === '-' || s === '='
+}
